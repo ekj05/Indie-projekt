d9f4eaf885f5e0d6cae4db76ef194fd51e639745
ekj05 <smolf0505@gmail.com>
2023-12-07 09:13:12 +0100

    commit

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 const radio = document.querySelectorAll('input[name="imgselect"]')
 radio[0].checked = true
-check()
+checkradio()
 
 function checkradio(){
     img1.classList.remove("show")
     img2.classList.remove("show")
     img3.classList.remove("show")
-    if(radio[0].checked === true){
-        img1.classList.add("show")
-    } else if(radio[1].checked === true){
-        img2.classList.add("show")
-    } else if(radio[2].checked === true){
-        img3.classList.add("show")
+    switch (true) {
+        case radio[0].checked:
+            img1.classList.add("show");
+            break;
+        case radio[1].checked:
+            img2.classList.add("show");
+            break;
+        case radio[2].checked:
+            img3.classList.add("show");
+            break;
     }
 }
 
 function handleclick(){
-    
+    switch(true){
+        case radio[0].checked:
+            radio[2].checked = true
+            break;
+        case radio[1].checked:
+            radio[0].checked = true
+            break;
+        case radio[2].checked:
+            radio[1].checked = true
+            break;
+    }
+    checkradio()
 }
 
+function handleclick2(){
+    switch(true){
+        case radio[0].checked:
+            radio[1].checked = true
+            break;
+        case radio[1].checked:
+            radio[2].checked = true
+            break;
+        case radio[2].checked:
+            radio[0].checked = true
+            break;
+    }
+    checkradio()
+}
 
-radio[0].addEventListener("change", checkradio)
-radio[1].addEventListener("change", checkradio)
-radio[2].addEventListener("change", checkradio)
+radio[0].addEventListener("click", checkradio)
+radio[1].addEventListener("click", checkradio)
+radio[2].addEventListener("click", checkradio)
 arrowL.addEventListener("click", handleclick)
-// arrowR.addEventListener("click", handleclick)
+arrowR.addEventListener("click", handleclick2)
